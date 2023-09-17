573602bb7f594279d5f4f22594131ae3ff6f7190
Paulo Nascimento <phin01@gmail.com>
2023-09-17 19:49:59 -0300

    feat: include support for appsettings.json

--- ContactCompiler/Program.cs
+++ ContactCompiler/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddJsonFile("appsettings.json", optional: true);
+        config.AddJsonFile("local.settings.json", optional: true);
+        config.AddEnvironmentVariables();
+    })
     .Build();
 
 host.Run();
