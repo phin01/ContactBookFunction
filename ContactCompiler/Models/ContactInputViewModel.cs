b80331683b75bfe088a7162c1e4be7c1e4d5932f
Paulo Nascimento <phin01@gmail.com>
2023-09-18 19:17:09 -0300

    feat: create models in function

---
+++ ContactCompiler/Models/ContactInputViewModel.cs
@@ -0,0 +1,9 @@
+﻿using DataAccessLibrary.Models;
+
+namespace ContactBook.Models;
+
+public class ContactInputViewModel : IContactInputModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
---
+++ ContactCompiler/Models/ContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLibrary.Models;
+
+namespace ContactBook.Models;
+
+public class ContactViewModel : IContactModel
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
