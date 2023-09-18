eb032a5c70cba5091ef61fd95647ab3ba9d81ff4
Paulo Nascimento <phin01@gmail.com>
2023-09-18 19:18:14 -0300

    feat: create initial logic for addContact and listContact functions

--- ContactCompiler/ContactCompiler.cs
+++ ContactCompiler/ContactCompiler.cs
@@ -1,4 +1,9 @@
+using System;
 using System.Net;
+using System.Text.Json;
+using ContactBook.Models;
+using DataAccessLibrary.Data;
+using DataAccessLibrary.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -8,16 +13,44 @@ namespace ContactCompiler
     public class ContactCompiler
     {
         private readonly ILogger _logger;
+        private readonly IDatabase _db;
 
-        public ContactCompiler(ILoggerFactory loggerFactory)
+        public ContactCompiler(ILoggerFactory loggerFactory, IDatabase db)
         {
             _logger = loggerFactory.CreateLogger<ContactCompiler>();
+            _db = db;
         }
 
         [Function("AddContacts")]
-        public HttpResponseData AddContacts([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> AddContacts([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
+            HttpResponseData response;
+            List<ContactInputViewModel> contacts;
+
+            try
+            {
+                contacts = await JsonSerializer.DeserializeAsync<List<ContactInputViewModel>>(req.Body);
+                _db.AddContacts(contacts);
+            }
+            catch (Exception)
+            {
+                response = req.CreateResponse(HttpStatusCode.BadRequest);
+                response.WriteString("Incorrect input format");
+                return response;
+            }
+
+            response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString($"{contacts.Count} contacts Added to Contact Book");
+            return response;
+        }
+
+        [Function("ListContacts")]
+        public async Task<HttpResponseData> ListContactsAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            var contacts = _db.GetAllContacts().ToList();
+
             var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(contacts);
             return response;
         }
 
--- ContactCompiler/Program.cs
+++ ContactCompiler/Program.cs
@@ -1,4 +1,7 @@
+using ContactBook.Controllers;
+using DataAccessLibrary.Data;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
@@ -8,6 +11,11 @@ var host = new HostBuilder()
         config.AddJsonFile("appsettings.json", optional: true);
         config.AddJsonFile("local.settings.json", optional: true);
         config.AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IContactValidator, ContactValidator>();
+        services.AddSingleton<IDatabase, MockDatabase>();
     })
     .Build();
 
